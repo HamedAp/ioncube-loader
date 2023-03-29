31c68a58494cb44212a16fd1870d8f46dfb8e0e5
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-03-29 12:45:03 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
+
+if command -v apt-get >/dev/null; then
 wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz -C /usr/local
 sudo rm -rf ioncube_loaders_lin_x86-64.tar.gz
 PHPVERSION=$(php -i | grep /.+/php.ini -oE | sed 's/[^0-9.]*//g')
 echo "zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION}so" > /etc/php/${PHPVERSION::-1}/apache2/conf.d/00-ioncube.ini
 systemctl restart apache2
+elif command -v yum >/dev/null; then
+wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz -C /usr/lib64/php/modules
+echo "zend_extension = zend_extension = /usr/lib64/php/modules/ioncube/ioncube_loader_lin_7.4.so" > /etc/php.d
+systemctl restart httpd
+
+
+
+fi
