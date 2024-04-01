8927d45d906ecb4f8e12c5ff244a778412cdfca0
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2024-04-02 00:20:16 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 
 if command -v apt-get >/dev/null; then
-wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+wget -4 https://github.com/HamedAp/ioncube-loader/releases/download/1.0/ioncube_loaders_lin_x86-64.tar.gz
 sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz -C /usr/local
 sudo rm -rf ioncube_loaders_lin_x86-64.tar.gz
 PHPVERSION=$(php -i | grep /.+/php.ini -oE | sed 's/[^0-9.]*//g')
@@ -9,7 +9,7 @@ sed -i 's@zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION::-
 echo "zend_extension = /usr/local/ioncube/ioncube_loader_lin_${PHPVERSION}so" >> /etc/php/${PHPVERSION::-1}/cli/php.ini
 systemctl restart apache2
 elif command -v yum >/dev/null; then
-wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+wget -4 https://github.com/HamedAp/ioncube-loader/releases/download/1.0/ioncube_loaders_lin_x86-64.tar.gz
 sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz -C /usr/lib64/php/modules
 PHPVERSION=$(php -r 'echo PHP_VERSION;')
 echo "zend_extension = /usr/lib64/php/modules/ioncube/ioncube_loader_lin_${PHPVERSION::-3}.so" > /etc/php.d/00-ioncube.ini
@@ -21,7 +21,7 @@ fi
 
 if [ $(uname -m) = "aarch64" ]; then
     arch="arm64"
-    wget -4 https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_aarch64.tar.gz
+    wget -4 https://github.com/HamedAp/ioncube-loader/releases/download/1.0/ioncube_loaders_lin_aarch64.tar.gz
     sudo tar xzf ioncube_loaders_lin_aarch64.tar.gz -C /usr/lib/php/20210902
 sudo rm -rf ioncube_loaders_lin_aarch64.tar.gz
 PHPVERSION=$(php -i | grep /.+/php.ini -oE | sed 's/[^0-9.]*//g')
